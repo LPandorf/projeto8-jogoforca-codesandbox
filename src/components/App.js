965468971314c57bd449bd5849800fb060cdc5f2
LPandorf <106892978+LPandorf@users.noreply.github.com>
2022-09-25 20:27:28 -0300

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+//importações
+//.import Palavras
+
+//imagens
+
+//exportação
+export default function App() {
+  return (
+    <div class="App">
+      <div class="lado">
+        <img src="./img/forca0.png" />
+        <div class="coluna">
+          <div class="escolherpalavra" button="escolherpalavra">
+            Escolher Palavra
+          </div>
+          <div class="palavra">_ _ _ _ _</div>
+        </div>
+      </div>
+    </div>
+  );
+}
